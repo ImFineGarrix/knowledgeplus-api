4f8cb978f30004b21694b29de05bcdf5431587c0
jakarin <jakarin.b@mail.kmutt.ac.th>
2023-11-19 06:24:37 +0700

    Jakarin
    Edit env

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -7,8 +7,11 @@ import (
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
+	err := godotenv.Load(".env.local")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 }
