68ed6d9898bb7023bbefa0e77e833cc0a4f2083c
jakarin <jakarin.b@mail.kmutt.ac.th>
2023-11-18 20:37:15 +0700

    Jakarin
    First Commit

---
+++ array/array.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+var productname [4]string
+
+func main() {
+	fmt.Println(productname)
+	price := [4]float32{10,20,30,40}
+	fmt.Println(price)
+}
---
+++ slice/slice.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var courseName []string
+	courseName = []string{"java","python"}
+	fmt.Println(courseName)
+}
