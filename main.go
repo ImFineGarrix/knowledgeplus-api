1f85563b57150cfb8297a698c17f287c0112aa2a
jakarin <jakarin.b@mail.kmutt.ac.th>
2023-11-22 22:07:56 +0700

    Change cors

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gin-contrib/cors"
 )
 
 // func init() {
@@ -19,7 +18,7 @@ func main() {
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
-	r.Use(cors.Default())
+	r.Use(corsMiddleware())
 
 	defaultPath := r.Group("/api")
 
@@ -54,4 +53,19 @@ func setupRouter() *gin.Engine {
 	defaultPath.GET("/courses/:id", CourseRepo.GetCourse)
 
 	return r
+}
+
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
