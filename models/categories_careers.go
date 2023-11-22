fd0f2fd0135b59f378a75e2c0b192268245c5f0b
jakarin <jakarin.b@mail.kmutt.ac.th>
2023-11-22 22:50:03 +0700

    edit category and career models

--- models/categories_careers.go
+++ models/categories_careers.go
@@ -1,9 +1,7 @@
 package models
 
-import "gorm.io/gorm"
-
 type CareerCategory struct {
-	CategoriesCareersID int `gorm:"primaryKey;autoIncrement" json:"categories_careers_id"`
+	CategoriesCareersID int `gorm:"primaryKey;autoIncrement"`
 	CareerID            int `gorm:"foreignKey:CareerID ;references:CareerID "`
 	CategoryID          int `gorm:"foreignKey:CategoryID ;references:CategoryID "`
 }
@@ -12,11 +10,11 @@ func (CareerCategory) TableName() string {
 	return "Categories_Careers"
 }
 
-// CreateCareer creates a new Career record in the database.
-func CreateCareerCategory(db *gorm.DB, careerCategory *CareerCategory) (err error) {
-	err = db.Create(careerCategory).Error
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// // CreateCareer creates a new Career record in the database.
+// func CreateCareerCategory(db *gorm.DB, careerCategory *CareerCategory) (err error) {
+// 	err = db.Create(careerCategory).Error
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
