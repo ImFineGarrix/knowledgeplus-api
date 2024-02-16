6f3d5bda11a012f16267a5bc68b0f35323ee67d1
jakarin <jakarin.b@mail.kmutt.ac.th>
2024-02-17 03:24:11 +0700

    edit pointer career in course

--- models/carrers.go
+++ models/carrers.go
@@ -31,8 +31,8 @@ type SkillsLevelsInCareers struct {
 	KnowledgeDesc  string          `gorm:"column:knowledge_desc;" json:"knowledge_desc"`
 	AbilityDesc    string          `gorm:"column:ability_desc;" json:"ability_desc"`
 	LevelID        int             `gorm:"column:level_id; not null" json:"level_id"`
-	CourseID       *int            `gorm:"column:course_id; not null;" json:"-"`
-	CareerID       *int            `gorm:"column:career_id; not null;" json:"-"`
+	CourseID       *int            `gorm:"column:course_id;" json:"-"`
+	CareerID       *int            `gorm:"column:career_id;" json:"-"`
 	Skill          SkillInCareers  `gorm:"foreignKey:SkillID;references:SkillID" json:"skill"`
 	Course         CourseInCareers `gorm:"foreignKey:CourseID;references:CourseID" json:"courses"`
 }
